1597cf71e1aaad4ec7028ced926c2a043f732c47
Ying <1186488719@qq.com>
2020-08-27 17:46:57 +0800

    删除win32

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -6,6 +6,7 @@
 
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
+	QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
 	QLibrary lib("./MainLogicFrame.dll");
 	if (lib.load()) {
 		QFunctionPointer fun = lib.resolve("Handle");
